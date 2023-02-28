3cdce636c3d15622e5cd05361ea13f4942d5f8f0
윤영훈YoungHun Yun <google-oauth2|102604445318989132038>
2023-02-28 05:35:09 +0000

    2nd commit

---
+++ includes/common.js
@@ -0,0 +1,5 @@
+// Read more in the JS API documentation here: https://docs.dataform.co/guides/javascript/js-api
+
+// publish("new_table")
+//   .type("table")
+//   .query(ctx => `select * from ${ctx.ref("source_table")}`);
