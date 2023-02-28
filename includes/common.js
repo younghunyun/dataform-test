863d750a503da9e886d7b63198b6c43e543bb8d5
윤영훈 <younghun.yun@bespinlab.kr>
2023-02-28 06:15:24 +0000

    create js to get uuid string

--- includes/common.js
+++ includes/common.js
@@ -1,5 +1,10 @@
-// Read more in the JS API documentation here: https://docs.dataform.co/guides/javascript/js-api
-
-// publish("new_table")
-//   .type("table")
-//   .query(ctx => `select * from ${ctx.ref("source_table")}`);
+var characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+var uuid = '';
+for (var i = 0; i < 4; i++) {
+    var randomString = '';
+    for (var j = 0; j < 4; j++) {
+        randomString += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    uuid = (i < 3) ? uuid + randomString + '-' : uuid + randomString;
+}
+module.exports = { uuid };
